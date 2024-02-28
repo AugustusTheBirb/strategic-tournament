550ad021c93475a53bb7fb3f6e832479f0e54c10
AugustusTheBirb <72280484+AugustusTheBirb@users.noreply.github.com>
2024-02-28 15:43:39 +0200

    fixed example.py

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 import game
-import strategies
+import strategies as str
 
-players = [allDove,allHawk,rando,copycat] #add the strategies you want to play in the tournament to this list
+players = [str.allDove,str.allHawk,str.rando,str.copycat] #add the strategies you want to play in the tournament to this list
 
 rounds = 100 #numbers of rounds in one game
 retries = 100 #the retries are so that non-deterministic strategies's payoffs get averaged out
 matrix  = [[(5,5),(2,7)],
           [(7,2),(0,0)]]
 
-full_game(players,rounds,retries,matrix)
+game.full_game(players,rounds,retries,matrix)
