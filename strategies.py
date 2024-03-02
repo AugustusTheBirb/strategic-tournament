07e249595d686861a233f7ae5142e8f5d4d4d62a
LeMeowM <lemeowmew@gmail.com>
2024-03-02 13:18:56 +0100

    fixed + added some optimisations

--- strategies.py
+++ strategies.py
@@ -19,13 +19,15 @@ def hawks_in_last_k_opmoves(opmoves, k):
 
 # strategies themselves
 class randStrat:
-    def rando(self, mymoves, opmoves):  # 50/50 hawk/dove | Augustas
+    @staticmethod
+    def rando(mymoves, opmoves):  # 50/50 hawk/dove | Augustas
         if random.random() > 0.5:
             return "dove"
         else:
             return "hawk"
 
-    def believer(self, my, op):  # tiki praeitimi, renkasi ta move kuris anksciau geras jam buvo | Tomas
+    @staticmethod
+    def believer(my, op):  # tiki praeitimi, renkasi ta move kuris anksciau geras jam buvo | Tomas
         matrix = [[(5, 5), (2, 7)], [(7, 2), (0, 0)]]
         d = {'dove': 0, 'hawk': 1}
         if len(my) < 10:
@@ -41,7 +43,8 @@ class randStrat:
         else:
             return 'dove'
 
-    def intrusive_thought(self, mymoves,
+    @staticmethod
+    def intrusive_thought(mymoves,
                           opmoves):  # tit-for-tat but if it gets too quiet it will randomly punch for shits and giggles
         if len(mymoves) == 0:
             return "dove"
@@ -51,20 +54,23 @@ class randStrat:
         return opmoves[-1]
 
 
-
 class detStrat:
-    def allDove(self, mymoves, opmoves):  # always dove | Augustas
+    @staticmethod
+    def allDove(mymoves, opmoves):  # always dove | Augustas
         return "dove"
 
-    def allHawk(self, mymoves, opmoves):  # always hawk | Augustas
+    @staticmethod
+    def allHawk(mymoves, opmoves):  # always hawk | Augustas
         return "hawk"
 
-    def copycat(self, mymoves, opmoves):  # copies opponents last move | Vasaris
+    @staticmethod
+    def copycat(mymoves, opmoves):  # copies opponents last move | Vasaris
         if len(mymoves) == 0:
             return "dove"
         return opmoves[-1]
 
-    def conformist(self, mymoves, opmoves):  # uses the most popular move | Lukas
+    @staticmethod
+    def conformist(mymoves, opmoves):  # uses the most popular move | Lukas
         if len(mymoves) == 0:
             return "dove"
         hawk_count = 0
@@ -78,7 +84,8 @@ class detStrat:
         else:
             return "dove"
 
-    def critic(self, mymoves, opmoves):  # reverses opponents last move | Lukas
+    @staticmethod
+    def critic(mymoves, opmoves):  # reverses opponents last move | Lukas
         if len(mymoves) == 0:
             return "dove"
         if opmoves[-1] == "hawk":
@@ -86,7 +93,8 @@ class detStrat:
         else:
             return "hawk"
 
-    def rebel(self, mymoves, opmoves):  # uses the least popular move | Lukas
+    @staticmethod
+    def rebel(mymoves, opmoves):  # uses the least popular move | Lukas
         if len(mymoves) == 0:
             return "dove"
         hawk_count = 0
@@ -100,23 +108,24 @@ class detStrat:
         else:
             return "hawk"
 
-    def pushover(self, mymoves, opmoves):
+    @staticmethod
+    def pushover(mymoves, opmoves):
         if len(opmoves) < 3:
             return "hawk"
         if "dove" in opmoves[-3:]:
             return "hawk"
         return "dove"
 
-    def predator(self, mymoves, opmoves):  # always dove unless last 3 opponents were doves | Lukas
+    @staticmethod
+    def predator(mymoves, opmoves):  # always dove unless last 3 opponents were doves | Lukas
         if len(opmoves) < 2:
             return "hawk"
         if "hawk" in opmoves[-3:]:
             return "dove"
         return "hawk"
 
-
-
-    def alzheimer_conformist(self, mymoves,
+    @staticmethod
+    def alzheimer_conformist(mymoves,
                              opmoves):  # uses the most popular move in the last five moves | Mokslo gildija
         if len(opmoves) > 5:
             hawk_count = 0
@@ -127,7 +136,8 @@ class detStrat:
                 return "hawk"
         return "dove"
 
-    def sore_loser(self, mymoves, opmoves):  # if it has fewer points its opponent it goes hawk, else dove | Mykolas
+    @staticmethod
+    def sore_loser(mymoves, opmoves):  # if it has fewer points its opponent it goes hawk, else dove | Mykolas
         myscore = 0
         opscore = 0
         for i in range(len(mymoves)):
@@ -138,12 +148,14 @@ class detStrat:
         else:
             return "dove"
 
-    def grudge(self, mymoves, opmoves):  # plays dove until the opponent plays a hawk then keeps playing hawk | meow
+    @staticmethod
+    def grudge(mymoves, opmoves):  # plays dove until the opponent plays a hawk then keeps playing hawk | meow
         if "hawk" in opmoves:
             return "hawk"
         return "dove"
 
-    def prod(self, mymoves, opmoves):  # play hawk, if no hawks play hawk every third turn else tit-for-tat | meow
+    @staticmethod
+    def prod(mymoves, opmoves):  # play hawk, if no hawks play hawk every third turn else tit-for-tat | meow
         if len(mymoves) == 0:
             return "hawk"
         if len(mymoves) < 3:
@@ -155,11 +167,10 @@ class detStrat:
             return "hawk"
         return "dove"
 
-    def tit_for_tattat(self, mymoves, opmoves):  # smacks back if it gets smacked twice in a row | meow
+    @staticmethod
+    def tit_for_tattat(mymoves, opmoves):  # smacks back if it gets smacked twice in a row | meow
         if len(opmoves) < 2:
             return "dove"
         if "dove" not in opmoves[-2:]:
             return "hawk"
         return "dove"
-
-
